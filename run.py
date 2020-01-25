c8ef935febb6d5f9ce1a87b1d9fc4a869c42a164
Iheb-Haboubi <iheb.haboubi56@gmail.com>
2020-01-25 21:23:59 +0100

    add get_username function

--- run.py
+++ run.py
@@ -2,11 +2,35 @@ from helpers import make_folder
 from scraper import InstagramScraper
 
 
+def get_username():
+
+    prompt = input("insert instagram username: ")
+    # remove spaces from user input
+    prompt = prompt.strip()
+    if prompt == "":
+        print("Error : username cannot be empty. Please use a valid username")
+        quit()
+
+    if len(prompt) > 30:
+        print("Error : username is too long. Please use a valid username")
+        quit()
+
+    for char in prompt:
+        if not (char.isalnum() or char == "." or char == "_"):
+            print(
+                "Error : username can only contain letters, numbers, periods, and underscores. Please use a valid username"
+            )
+            quit()
+
+    return prompt
+
+
 def main():
-    scraper = InstagramScraper()
+    username = get_username()
+    scraper = InstagramScraper(username)
     scraper.get_query_hash()
     scraper.scrape()
-    make_folder(scraper.username)
+    make_folder(username)
     scraper.download_images()
 
 
@@ -16,6 +40,7 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         print("\nKeyboard interrupt : Instagram Scraper stopped.")
 
+    # check for dependencies
     except (NameError, ModuleNotFoundError):
         print("\nError : Please check that all necessary dependencies are installed.")
 
